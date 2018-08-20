ab4066a29ed63b430bf38022737fbb52ea4dca7e
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-08-21 03:25:24 +0900

    Renamed Repository.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "heroku_test_app_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "blogfav_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Settings for the mailer
